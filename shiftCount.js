0f5698bae6d5b97accf0e73e6e73dae420249fd8
Habib <71340861+Habib2959@users.noreply.github.com>
2022-07-06 02:48:16 +0600

    Add files via upload

---
+++ shiftCount.js
@@ -0,0 +1,29 @@
+function shiftCount(data, n) {
+    if (n === 0) {
+        return [];
+    }
+    let countObj = {};
+    let numbersTBeRemoved = [];
+    // counting the repeated number
+    for (let i = 0; i < data.length; i++) {
+        if (!countObj[data[i]]) {
+            countObj[data[i]] = 1;
+        } else {
+            countObj[data[i]]++;
+        }
+    }
+    // separating the numbers which in repeated more than n times
+    for (const key in countObj) {
+        if (countObj[key] > n) {
+            numbersTBeRemoved.push(parseInt(key));
+        }
+    }
+    if (numbersTBeRemoved.length === 0) {
+        return data;
+    }
+    // removing the duplicate items
+    const result = data.filter((el) => !numbersTBeRemoved.includes(el));
+    return result;
+}
+
+console.log(shiftCount( [1, 2, 3], 6));
